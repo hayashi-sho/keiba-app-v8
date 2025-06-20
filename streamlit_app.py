aa3a3ab65561d151b7ceeac0fcb1055b275f0ea6
hayashi-sho <vov.vov1103@Outlook.com>
2025-06-21 06:16:22 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🏇 Keiba AI指数アプリ v8")
+st.write("出馬表URLを選択・自動取得し、AI指数と過去走比較を表示します。")
